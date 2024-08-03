a6636a90c437c200694768a5288ce3f8eec9d69f
MarmoPL <77023982+MarmoPL@users.noreply.github.com>
2024-08-03 22:57:47 +0200

    add new types and plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,24 @@
 module.exports = ({ env }) => ({
+  "custom-api": {
+    enabled: true,
+  },
+  io: {
+    enabled: true,
+    config: {
+      // This will listen for all supported events on the article content type
+      contentTypes: ["*"],
+
+      socket: {
+        serverOptions: {
+          cors: {
+            // origin: ["*", "http://localhost:1337", "http://localhost:3001"],
+            origin: "*",
+            methods: ["GET", "POST"],
+          },
+        },
+      },
+    },
+  },
   'users-permissions': {
     config: {
       jwtSecret: env('JWT_SECRET'),
